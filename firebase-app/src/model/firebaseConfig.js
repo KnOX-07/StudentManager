7f562fc70d324307e000661c603076441e2f5288
_ruP4L <114283758+KnOX-07@users.noreply.github.com>
2025-09-05 03:27:16 +0530

    Update firebaseConfig.js

--- firebase-app/src/model/firebaseConfig.js
+++ firebase-app/src/model/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD0pHrmUW4kclIRydCPurHC-AfDr5ooSvQ",
-    authDomain: "fir-app-5bfb9.firebaseapp.com",
-    projectId: "fir-app-5bfb9",
-    storageBucket: "fir-app-5bfb9.firebasestorage.app",
-    messagingSenderId: "952994635766",
-    appId: "1:952994635766:web:f4c6e9bde4330127e031d7",
-    measurementId: "G-ZJZRRBTMLM"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
