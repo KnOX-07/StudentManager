aef8a3cc4fffdd1adf82b8c4b00c8d172e231232
_ruP4L <114283758+KnOX-07@users.noreply.github.com>
2025-09-05 03:28:51 +0530

    Update firebaseConfig.js

--- firebase-app/src/model/firebaseConfig.js
+++ firebase-app/src/model/firebaseConfig.js
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "",
-    authDomain: "",
+    authDomain: " ",
     projectId: "",
     storageBucket: "",
     messagingSenderId: "",
